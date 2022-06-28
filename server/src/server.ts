2fe656b532e4d40c6892ea0810b1c7bde0fd86a9
caleb-freitas <calebfreitas@tutanota.com>
2022-06-28 02:47:44 -0300

    chore: added resolvers to apollo server

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,14 +1,17 @@
 import {ApolloServer} from 'apollo-server';
 import {typeDefs} from './schema';
+import {resolvers} from './resolvers';
 
 const server = new ApolloServer({
   typeDefs,
+  resolvers,
+  csrfPrevention: true,
+  cache: 'bounded',
 });
 
 server.listen().then(() => {
   console.log(`
-    Server is running!
-    Listening on port 4000
+    Server is running on port 4000!
     Explore at https://studio.apollographql.com/sandbox
   `);
 });
