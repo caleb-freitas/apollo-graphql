100d25e24d03790b7e09fea3fb68b9f28c4ed8f5
caleb-freitas <calebfreitas@tutanota.com>
2022-06-28 03:25:41 -0300

    feat: added get okrs query pagination

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,5 +1,30 @@
+import { gql, useQuery } from "@apollo/client"
+
+const GetOkrsQuery = gql`
+  query GetOkrsQuery {
+    okrs {
+      id
+      title
+    }
+  }
+`
+
+type GetOkrs = {
+  okrs: {
+    id: string
+    title: string
+  }[]
+}
+
 export function App() {
+  const { data } = useQuery<GetOkrs>(GetOkrsQuery)
   return (
-    <h1>Hello, world</h1>
+    <div>
+      {data?.okrs.map(okr => {
+        return (
+          <p key={okr.id}>{okr.title}</p>
+        )
+      })}
+    </div>
   )
 }
--- client/src/main.tsx
+++ client/src/main.tsx
@@ -1,9 +1,13 @@
+import { ApolloProvider } from '@apollo/client'
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import {App} from './App'
+import { apolloClient } from './lib/apollo-client'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <App />
+    <ApolloProvider client={apolloClient}>
+      <App />
+    </ApolloProvider>
   </React.StrictMode>
 )
